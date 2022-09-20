37f48ac3b486ca9ec075f2ce30da3fe834d89116
Mustafa Akgül <64050563+akgulmustafa@users.noreply.github.com>
2022-09-20 22:49:29 +0300

    Add files via upload

---
+++ javademos/arraysDemo/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] ogrenciler = new String[3];
+        ogrenciler[0]="Engin";
+        ogrenciler[1]="Derin";
+        ogrenciler[2]="Salih";
+
+        for(int i=0;i<ogrenciler.length;i=i+1){
+            System.out.println(ogrenciler[i]);
+        }
+    }
+}
---
+++ javademos/loopDemo/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i=i+1){
+            System.out.println(i);
+        }
+        System.out.println("For Döngüsü Bitti");
+
+        ///while
+        int  i=1;
+        while(i<10){
+            System.out.println(i);
+            i+=2;
+        }
+        System.out.println("While Döngüsü Bitti");
+    }
+}
